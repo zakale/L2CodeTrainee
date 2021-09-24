42e3f3fd58dab33452eec074fc5807ab17846dc3
zakale <vieiralopesalex@gmail.com>
2021-09-23 22:53:54 -0300

    entrega das pastas e comentários

---
+++ SQL/prova.sql
@@ -0,0 +1,10 @@
+-- Listar a quantidade de horas que cada professor tem em aula
+
+SELECT COUNT(numhoras) AS qtdehoras, numhoras FROM HORAS GROUP BY numhoras;
+
+-- Lista a salas ocupadas e vazias
+
+SELECT SALA.numsala, HORARIO.horainicio, HORARIO.diasem
+FROM HORARIO 
+INNER JOIN SALA ON HORARIO.numsala = SALA.numsala 
+INNER JOIN HORARIO ON HORARIO.numhoras_HORARIO = HORARIO.horaincio;
